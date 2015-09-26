fdef66d5b3c53ff3d5fe8b5f7f2d03bf2ea31ba0
Jason Chen <jasonbiochem@gmail.com>
2015-09-26 16:00:53 -0400

    Sample code
    Renamed files only

---
+++ FtpOverUdp/UdpServer/Thread.cpp
@@ -0,0 +1,41 @@
+/*************************************************************************************
+*								 File Name	: Thread.cpp		   			 	     *
+*	Usage : Used for threads creation                                                *
+**************************************************************************************/
+#include <cstdio>
+#include <iostream>
+#include "Thread.h"
+using namespace std;
+
+/**
+ * Function - pthread_callback
+ * Usage: This is the callback needed by the Thread class
+ *
+ * @arg: void *
+ */
+void * Thread::pthread_callback (void *ptrThis)
+{
+
+    if ( ptrThis == NULL )
+	     return NULL ;
+    Thread  * ptr_this =(Thread *)(ptrThis) ;
+    ptr_this->run(); /* Starts the created thread */
+    return NULL;
+}
+
+/**
+ * Function - start
+ * Usage: Thread Creation
+ *
+ * @arg: void
+ */
+void Thread::start ()
+{
+	int result;
+	/* Create the thread specifying the stack size and the callback function */
+    if((result = _beginthread((void (*)(void *))Thread::pthread_callback,STKSIZE,this ))<0)
+	{
+		cerr << "_beginthread error " << endl;
+		exit(1);
+	}
+}
