916f6a78bd58e6a0ac90e700d8be97f1b2846f63
KaichenZhang <ac1102919258@gmail.com>
2015-10-07 01:28:36 -0400

    Changed the process flow and Tcp feathers.

--- FtpOverUdp/UdpClient/FtpClient.cpp
+++ FtpOverUdp/UdpClient/FtpClient.cpp
@@ -1,380 +1,277 @@
 /*************************************************************************************
-*								 File Name	: Server.cpp		   			 	     *
-*	Usage : Handles Client request for Uploading, downloading and listing of files   *
+*								 File Name	: Client.cpp		   			 	     *          *
+*	Usage : sendtos request to Server for Uploading, downloading and listing of files  *
 **************************************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
-#include <winsock.h>
-#include <iostream>
-#include <windows.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <process.h>
-#include "Thread.h"
-#include "FtpClient.h"
-#include <stdlib.h>
-
+#include "Client.h"
 using namespace std;
- 
+
+
+
 /**
-* Constructor - FtpClient
-* Usage: Initialize the socket connection
+* Function - main
+* Usage: Initiates the Client
+*
+* @arg: int, char*
+*/
+int main(int argc, char *argv[])
+{
+	UdpClient * tc = new UdpClient();
+	tc->startClient();
+	while (1)
+	{
+		tc->showMenu();
+	}
+
+	return 0;
+}
+
+
+/**
+* Function - startClient
+* Usage: Initialize WinSocket and get the host name and server IP Address to connnect
 *
 * @arg: void
 */
-FtpClient::FtpClient()
+void UdpClient::startClient()
 {
-	nextClientPort = REQUEST_PORT + 1;
-	WSADATA wsadata;
-	/* Initialize Windows Socket information */
-	if (WSAStartup(0x0202, &wsadata) != 0)
+	/* Initialize WinSocket */
+	if (WSAStartup(0x0202, &wsaData) != 0)
 	{
-		cerr << "Starting WSAStartup() error\n" << endl;
-		exit(1);
+		WSACleanup();
+		cerr << "Error in starting WSAStartup()";
+		return;
 	}
 
+	/* Get Host Name */
+	if (gethostname(hostName, HOSTNAME_LENGTH) != 0)
+	{
+		cerr << "can not get the host name,program ";
+		return;
+	}
+	cout << "ftp_Udp starting on host: " << hostName << endl;
+	cout << "Type name of ftp server: " << endl;
+	getline(cin, serverIpAdd);
+}
+
 /**
- * Function - ResolveName
- * Usage: Returns the binary, network byte ordered address
- *
- * @arg: string
- */
-unsigned long FtpClient::ResolveName(string name)
+* Constructor - UdpClient
+* Usage: Initialize the connection status
+*
+* @arg: void
+*/
+UdpClient::UdpClient()
+{
+	connectionStatus = true;
+}
+
+
+
+/**
+* Function - showMenu
+* Usage: Display the Menu with options for the User to select based on the operation
+*
+* @arg: void
+*/
+void UdpClient::showMenu()
+{
+	int optionVal;
+	cout << "1 : GET " << endl;
+	cout << "2 : EXIT " << endl;
+	cout << "Please select the operation that you want to perform : ";
+	/* Check if invalid value is provided and reset if cin error flag is set */
+	if (!(cin >> optionVal))
+	{
+		cout << endl << "Input Types does not match " << endl;
+		cin.clear();
+		cin.ignore(250, '\n');
+	}
+	/* Based on the option selected by User, set the transfer type and invoke the appropriate function */
+	switch (optionVal)
+	{
+	case 1:
+		transferType = "get";
+		run();
+		break;
+
+	case 2:
+		cout << "Terminating... " << endl;
+		exit(1);
+		break;
+
+	default:
+		cout << endl << "Please select from one of the above options " << endl;
+		break;
+	}
+	cout << endl;
+}
+
+
+
+/**
+* Function - run
+* Usage: Based on the user selected option invokes the appropriate function
+*
+* @arg: void
+*/
+void UdpClient::run()
+{
+
+	/* Socket creation */
+	if ((clientSock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) //create the socket
+	{
+		cerr << "Socket Creation Error";
+		return;
+	}
+
+
+	/* Establish connection with Server */
+	ServPort = REQUEST_PORT;
+	memset(&ServAddr, 0, sizeof(ServAddr));     /* Zero out structure */
+	ServAddr.sin_family = AF_INET;             /* Internet address family */
+	ServAddr.sin_addr.s_addr = ResolveName(serverIpAdd);   /* Server IP address */
+	ServAddr.sin_port = htons(ServPort); /* Server port */
+	
+	/* Socket Creation */
+	if ((clientSock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		cerr << "Socket Creation Error";
+		connectionStatus = false;
+		return;
+	}
+
+
+	/* Based on the Selected option invoke the appropriate function */
+	if (strcmp(transferType.c_str(), "get") == 0)
+	{
+		cin.ignore();
+		if (connectionStatus)
+		{
+			/* Initiate file retrieval */
+			sendtoMsg.type = REQ_GET;
+			getOperation();
+		}
+	}
+	else
+	{
+		cerr << "Wrong request type";
+		return;
+	}
+
+}
+
+
+/**
+* Destructor - ~UdpClient
+* Usage: DeAllocate the allocated memory
+*
+* @arg: void
+*/
+UdpClient::~UdpClient()
+{
+	/* When done uninstall winsock.dll (WSACleanup()) and return; */
+	WSACleanup();
+}
+
+
+
+
+
+/**
+* Function - ResolveName
+* Usage: Returns the binary, network byte ordered address
+*
+* @arg: string
+*/
+unsigned long UdpClient::ResolveName(string name)
 {
 	struct hostent *host;            /* Structure containing host information */
 
 	if ((host = gethostbyname(name.c_str())) == NULL)
 	{
-		cerr<<"gethostbyname() failed"<<endl;
+		cerr << "gethostbyname() failed" << endl;
 		return(1);
 	}
 
 	/* Return the binary, network byte ordered address */
-	return *((unsigned long *) host->h_addr_list[0]);
-}
-
-
-	/* Socket Creation */
-        if ((clientSock = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
-	{
-		std::cerr << "Socket Creation Error,exit" << endl;
-		exit(1);
-	}
-
-	
-
-/**
-* Destructor - ~FtpClient
-* Usage: DeAllocate the allocated memory
-*
-* @arg: void
-*/
-FtpClient::~FtpClient()
-{
-	closesocket(clientSock);
-	WSACleanup();
+	return *((unsigned long *)host->h_addr_list[0]);
 }
 
 /**
-* Function - start
-* Usage: Usage: Listen and handle the requests from server
-*
-* @arg: void
-*/
-void FtpClient::start()
-{
-	for (;;) /* Run forever */
-	{
-	  /* Get Host Name */
-	  if (gethostname(hostName, HOSTNAME_LENGTH) != 0)
-  	  {
-		cerr << "can not get the host name,program ";
-		return;
-	}
-	       cout << "Ftp starting on host: " << hostName << endl;
- 	       cout << "Type name of ftp server: " << endl;
-	       getline(cin, serverIpAdd);
-		
-		FtpThread * pt = new FtpThread(nextServerPort++);
-
-		// Wait for a request
-		pt->listen(serverSock, ServerAddr);
-
-		// Once request has arrived, start new thread so that server may receive another request
-		pt->start();
-	}
-}
-
-/*-------------------------------FtpThread Class--------------------------------*/
-/**
-* Function - listen
-* Usage: Blocks until incoming request message is captured
-*
-* @arg: void
-*/
-void FtpThread::listen(int sock, struct sockaddr_in initialSocket)
-{
-	/* Socket Creation */
-	if ((thrdSock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
-	{
-		std::cerr << "Socket Creation Error,exit" << endl;
-		exit(1);
-	}
-
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(inPort);
-
-	/* Binding the server socket to the Port Number */
-	if (::bind(thrdSock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-	{
-		cerr << "Socket Binding Error from FtpThread, exit" << endl;
-		exit(1);
-	}
-
-	curRqt = msgGet(sock, initialSocket);
-}
-
-/**
-* Function - getMsg
-* Usage: Blocks until the next incoming packet is completely received; returns the packet formatted as a message.
-*
-* @arg: void
-*/
-Msg* FtpThread::msgGet(SOCKET sock, struct sockaddr_in sockAddr)
-{
-	char buffer[512];
-	int bufferLength;
-	serverAddr = sockAddr;
-	memcpy(serverAddr.sin_zero, sockAddr.sin_zero, 8);
-	addrLength = sizeof(addr);
-
-	/* Check the received Message Header */
-	if ((bufferLength = recvfrom(sock, buffer, BUFFER_LENGTH, 0, (SOCKADDR *)&serverAddr, &addrLength)) == SOCKET_ERROR)
-	{
-		cerr << "recvfrom(...) failed when getting message" << endl;
-		exit(1);
-	}
-
-	// must destruct after use!
-	Msg* msg = new Msg();
-	memcpy(msg, buffer, MSGHDRSIZE);
-	memcpy(msg->buffer, buffer + MSGHDRSIZE, msg->length);
-	return msg;
-}
-
-/**
-* Function - msgSend
+* Function - msgsendto
 * Usage: Returns the length of bytes in msg_ptr->buffer,which have been sent out successfully
 *
 * @arg: int, Msg *
 */
-int FtpThread::msgSend(int sock, Msg * msg_ptr)
+int UdpClient::msgsendto(int clientSocket, Msg * msg_ptr)
 {
-	int n;
-	int expectedMsgLen = MSGHDRSIZE + msg_ptr->length;
-	if ((n = sendto(sock, (char *)msg_ptr, expectedMsgLen, 0, (SOCKADDR *)&serverAddr, addrLength)) != expectedMsgLen) {
-		std::cerr << "Send MSGHDRSIZE+length Error " << endl;
-		std::cerr << WSAGetLastError() << endl;
-		return(-1);
-	}
 
-	return (n - MSGHDRSIZE);
+	
+	int len;
+	if ((len = sendto(clientSocket, (char *)msg_ptr, MSGHDRSIZE + msg_ptr->length, 0, (SOCKADDR *)&ServAddr, addrLength)) != (MSGHDRSIZE + msg_ptr->length))
+	{
+		cerr << "sendto MSGHDRSIZE+length Error";
+		return(1);
+	}
+	/*Return the length of data in bytes, which has been sent out successfully */
+	return (len - MSGHDRSIZE);
+
 }
 
 /**
-* Function - run
-* Usage: Initiates client-terminated looping
+* Function - getOperation
+* Usage: Establish connection and retrieve file from server
 *
 * @arg: void
 */
-void FtpThread::run()
+void UdpClient::getOperation()
 {
-	currentSequenceNumber = serverIdentifier % SEQUENCE_RANGE;
-	handleCurrentMessage();
-
-	while (currentState != Terminated) {
-		curRqt = msgGet(thrdSock, addr);
-		handleCurrentMessage();
-	}
-
-	/* Close the connection and unlock the Mutex after successful transfer */
-	closesocket(thrdSock);
-}
-
-void FtpThread::handleCurrentMessage()
-{
-	/*Start receiving the request */
-	if (curRqt == NULL)
+	
+	
+	/* Get the hostname */
+	if (gethostname(reqMessage.hostname, HOSTNAME_LENGTH) != 0)
 	{
-		cerr << "Receive Req error,exit " << endl;
+		cerr << "can not get the host name " << endl;
+		return;
+	}
+	cout << "Type name of file to be retrieved: " << endl;
+	getline(cin, fileName);
+	strcpy(reqMessage.filename, fileName.c_str());
+	memcpy(sendtoMsg.buffer, &reqMessage, sizeof(reqMessage));
+	/* Include the length of the buffer */
+	sendtoMsg.length = sizeof(sendtoMsg.buffer);
+	cout << endl << endl << "Sent Request to " << serverIpAdd << ", Waiting... " << endl;
+	/* sendto the packed message */
+	numBytesSent = msgsendto(clientSock, &sendtoMsg);
+	if (numBytesSent == SOCKET_ERROR)
+	{
+		cout << "sendto failed.. Check the Message length.. " << endl;
 		return;
 	}
 
-	Msg* ask = NULL;
-
-	/* Check the type of operation and Construct the response and send to Server */
-	if (currentState == Initialized && curRqt->type == HANDSHAKE) {
-		ask = createServerHandshake();
-		currentState = HandshakeStarted;
-	}
-	else if (currentState == HandshakeStarted && curRqt->type == COMPLETE_HANDSHAKE) {
-		if (isHandshakeCompleted())
-			currentState = ReceivingResponse;
-	}
-	else if (currentState == ReceivingResponse && curRqt->type == REQ_GET) {
-		/*call get() function*/
-	}
-	else if (currentState == ReceivingResponse && curRqt->type == REQ_LIST) {
-		
-		/*call list() function*/
-	}
-	else if (currentState == Sending && curRqt->type == PUT) {
-		if (curRqt->sequenceNumber == currentSequenceNumber && !payloadData.empty()) {
-			// Iterate
-			currentSequenceNumber = (currentSequenceNumber + 1) % SEQUENCE_RANGE;
-			payloadData.pop(); // TODO: Manage memory?
-			if (payloadData.push())
-				currentState = ReceivingResponse;
-		}
-
-		ask = getNextChunk();
-	}
-	else if (currentState == ReceivingResponse && curRqt->type == TERMINATE)
-		currentState = Terminated;
-	else
-		cerr << "Invalid request header; ignored and waiting for next request" << endl;
-
-	if (ask != NULL) {
-		msgSend(thrdSock, ask);
-		delete ask;
-	}
-}
-
-Msg* FtpThread::createServerHandshake()
-{
-	Msg* handshakeAck = new Msg(*curRqt);
-	int lenClientData = 0;
-	while (isdigit(handshakeAck->buffer[lenClientData]))
-		lenClientData++;
-
-	std::string serverPart = ",";
-	serverPart += std::to_string((_ULonglong)serverIdentifier);
-	strcpy(&handshakeAck->buffer[lenClientData], serverPart.c_str());
-
-	return handshakeAck;
-}
-
-bool FtpThread::isHandshakeCompleted()
-{
-	return std::stoi(curRqt->buffer) == serverIdentifier;
-}
-
-bool FtpThread::tryLoadFile()
-{
-	// Empties out any data which may be remaining in the queue, if any
-	if (!payloadData.empty())
-		queue<char*>().swap(payloadData);
-
-	ifstream fileToRead;
-	fileToRead.open(string(filesDirectory).append(curRqt->buffer));
-	char* currentBuffer = NULL;
-	if (fileToRead.is_open()) {
-		while (!fileToRead.eof()) {
-			currentBuffer = new char[BUFFER_LENGTH];
-			memset(currentBuffer, '\0', BUFFER_LENGTH);
-			fileToRead.read(currentBuffer, BUFFER_LENGTH);
-
-			payloadData.push(currentBuffer);
-		}
-
-		// TODO: May need to insert EOF char!
-		return true;
-	}
-
-	return false;
-}
-
-void FtpThread::loadDirectoryContents()
-{
-	// Empties out any data which may be remaining in the queue, if any
-	if (!payloadData.empty())
-		queue<char*>().swap(payloadData);
-
-	// Code adapted from https://msdn.microsoft.com/en-us/library/windows/desktop/aa365200(v=vs.85).aspx
-	// Removed most of the error-checking --it is the responsibility of the person setting up the server to ensure that directories and files are set up correctly
-	WIN32_FIND_DATA ffd;
-	TCHAR szDir[MAX_PATH];
-	size_t length_of_arg;
-	HANDLE hFind = INVALID_HANDLE_VALUE;
-
-	char *buffer = new char[BUFFER_LENGTH];
-	int currIndex = 0;
-	memset(buffer, '\0', BUFFER_LENGTH);
-	hFind = FindFirstFile("files\\*", &ffd);
-	do
+	ofstream myFile(fileName, ios::out | ios::binary);
+	/* Retrieve the contents of the file and write the contents to the created file */
+	while ((numBytesRecvfrom = recvfrom(clientSock, receiveMsg.buffer, BUFFER_LENGTH, 0, (SOCKADDR *)&ServAddr, &addrLength))>0)
 	{
-		if (!(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+		/* If the file does not exist in the server, close the connection and exit */
+		if (strcmp(receiveMsg.buffer, "No such file") == 0)
 		{
-			for (int i = 0; i < sizeof(ffd.cFileName); i++, currIndex++) {
-				if (currIndex == BUFFER_LENGTH) {
-					payloadData.push(buffer);
-					buffer = new char[BUFFER_LENGTH];
-					memset(buffer, '\0', BUFFER_LENGTH);
-					currIndex = 0;
-				}
-
-				buffer[currIndex] = ffd.cFileName[i];
-				if (buffer[currIndex] == '\0')
-					i = sizeof(ffd.cFileName);
-			}
+			cout << receiveMsg.buffer << endl;
+			myFile.close();
+			remove(fileName.c_str());
+			closesocket(clientSock);
+			return;
 		}
-	} while (FindNextFile(hFind, &ffd) != 0);
-
-	payloadData.push(buffer);
+		else /* If the file exists, start reading the contents of the file */
+		{
+			myFile.write(receiveMsg.buffer, numBytesRecvfrom);
+			memset(receiveMsg.buffer, 0, sizeof(receiveMsg.buffer));
+		}
+	}
+	/* Close the connection after the file is received */
+	cout << "File received " << endl << endl;
+	myFile.close();
+	closesocket(clientSock);
 }
 
-Msg* FtpThread::getNextChunk()
-{
-	if (payloadData.empty())
-		return NULL;
 
-	currentState = Sending;
-	Msg* responseMsg = new Msg();
-	responseMsg->type = RESP;
-	responseMsg->length = BUFFER_LENGTH;
-	responseMsg->sequenceNumber = currentSequenceNumber;
-	memcpy(responseMsg->buffer, payloadData.front(), BUFFER_LENGTH);
-
-	return responseMsg;
-}
-
-Msg* FtpThread::getErrorMessage(const char* text)
-{
-	Msg* errorMsg = new Msg();
-	errorMsg->type = RESP_ERR;
-	errorMsg->length = BUFFER_LENGTH;
-	strcpy(errorMsg->buffer, text);
-
-	return errorMsg;
-}
-
-/**
-* Function - main
-* Usage: Initiates the Server
-*
-* @arg: void
-*/
-int main(void)
-{
-	FtpServer ts;
-	/* Start the server and start listening to requests */
-	ts.start();
-
-	return 0;
-}
